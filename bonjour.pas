ac5950130be4654f211f36e3201ff7eaad59fd30
ManohisoaS <manohisoa.randrianantenaina@gmail.com>
2022-12-04 18:59:02 +0300

    hellow world in pascal

---
+++ bonjour.pas
@@ -0,0 +1,17 @@
+program bonjour;
+var
+    a: integer;
+begin
+    a:= 40;
+    if a < 5
+    then begin
+        writeln('ok');
+        writeln('ol');
+    end
+    else
+    begin
+        writeln('end');
+    end;
+        
+    
+end.
